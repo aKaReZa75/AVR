7f55a20923b3b9a6126c911194b9a15209c7174b
Hossein Bagheri <akaReza75@gmail.com>
2024-02-03 14:05:27 +0330

    Update 7Segments_Oled.h

--- 7Segments_Oled.h
+++ 7Segments_Oled.h
@@ -7,77 +7,77 @@
  *
  */
 
-#define RGB_R_Config  DDRB
-#define RGB_R_Control PORTB
-#define RGB_R_Pin 3
+#define __RGB_R_Config  DDRB
+#define __RGB_R_Control PORTB
+#define __RGB_R_Pin 3
 
-#define RGB_G_Config  DDRD
-#define RGB_G_Control PORTD
-#define RGB_G_Pin 5
+#define __RGB_G_Config  DDRD
+#define __RGB_G_Control PORTD
+#define __RGB_G_Pin 5
 
-#define RGB_B_Config  DDRD
-#define RGB_B_Control PORTD
-#define RGB_B_Pin 6
+#define __RGB_B_Config  DDRD
+#define __RGB_B_Control PORTD
+#define __RGB_B_Pin 6
 
-#define SW1_Config      DDRD      
-#define SW1_Status      PIND
-#define SW1_Pin         2
-#define SW1_isPressed   bitCheck(SW1_Status, SW1_Pin)
+#define __SW1_Config      DDRD      
+#define __SW1_Status      PIND
+#define __SW1_Pin         2
+#define __SW1_isPressed   bitCheck(__SW1_Status, __SW1_Pin)
 
-#define SW2_Config      DDRD      
-#define SW2_Status      PIND
-#define SW2_Pin         3
-#define SW2_isPressed   bitCheck(SW2_Status, SW2_Pin)
+#define __SW2_Config      DDRD      
+#define __SW2_Status      PIND
+#define __SW2_Pin         3
+#define __SW2_isPressed   bitCheck(__SW2_Status, __SW2_Pin)
 
-#define SW3_Config      DDRB      
-#define SW3_Status      PINB
-#define SW3_Pin         5
-#define SW3_isPressed   bitCheck(SW3_Status, SW3_Pin)
+#define __SW3_Config      DDRB      
+#define __SW3_Status      PINB
+#define __SW3_Pin         5
+#define __SW3_isPressed   bitCheck(__SW3_Status, __SW3_Pin)
 
-#define _7Seg_A_Control PORTC
-#define _7Seg_A_Config  DDRC
-#define _7Seg_A_Pin     0
+#define ___7seg_A_Control PORTC
+#define ___7seg_A_Config  DDRC
+#define ___7seg_A_Pin     0
 
-#define _7Seg_B_Control PORTC
-#define _7Seg_B_Config  DDRC
-#define _7Seg_B_Pin     1
+#define ___7seg_B_Control PORTC
+#define ___7seg_B_Config  DDRC
+#define ___7seg_B_Pin     1
 
-#define _7Seg_C_Control PORTC
-#define _7Seg_C_Config  DDRC
-#define _7Seg_C_Pin     2
+#define __7seg_C_Control PORTC
+#define __7seg_C_Config  DDRC
+#define __7seg_C_Pin     2
 
-#define _7Seg_D_Control PORTC
-#define _7Seg_D_Config  DDRC
-#define _7Seg_D_Pin     3
+#define __7seg_D_Control PORTC
+#define __7seg_D_Config  DDRC
+#define __7seg_D_Pin     3
 
-#define _7Seg_E_Control PORTD
-#define _7Seg_E_Config  DDRD
-#define _7Seg_E_Pin     0
+#define __7seg_E_Control PORTD
+#define __7seg_E_Config  DDRD
+#define __7seg_E_Pin     0
 
-#define _7Seg_F_Control PORTD
-#define _7Seg_F_Config  DDRD
-#define _7Seg_F_Pin     1
+#define __7seg_F_Control PORTD
+#define __7seg_F_Config  DDRD
+#define __7seg_F_Pin     1
 
-#define _7Seg_G_Control PORTD
-#define _7Seg_G_Config  DDRD
-#define _7Seg_G_Pin     4
+#define __7seg_G_Control PORTD
+#define __7seg_G_Config  DDRD
+#define __7seg_G_Pin     4
 
-#define _7Seg_DP_Control PORTD
-#define _7Seg_DP_Config  DDRD
-#define _7Seg_DP_Pin     7
+#define __7seg_DP_Control PORTD
+#define __7seg_DP_Config  DDRD
+#define __7seg_DP_Pin     7
 
-#define _7Seg_Digit1_Control PORTB
-#define _7Seg_Digit1_Config DDRB
-#define _7Seg_Digit1_Pin 0
+#define __7seg_Digit1_Control PORTB
+#define __7seg_Digit1_Config DDRB
+#define __7seg_Digit1_Pin 0
 
-#define _7Seg_Digit2_Control PORTB
-#define _7Seg_Digit2_Config DDRB
-#define _7Seg_Digit2_Pin 1
+#define __7seg_Digit2_Control PORTB
+#define __7seg_Digit2_Config DDRB
+#define __7seg_Digit2_Pin 1
 
-#define _7Seg_Digit3_Control PORTB
-#define _7Seg_Digit3_Config DDRB
-#define _7Seg_Digit3_Pin 2
+#define __7seg_Digit3_Control PORTB
+#define __7seg_Digit3_Config DDRB
+#define __7seg_Digit3_Pin 2
 
-#define _7Seg_Digit4_Control PORTB
-#define _7Seg_Digit4_Config DDRB
-#define _7Seg_Digit4_Pin 4
+#define __7seg_Digit4_Control PORTB
+#define __7seg_Digit4_Config DDRB
+#define __7seg_Digit4_Pin 4
