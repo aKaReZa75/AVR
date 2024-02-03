29e822a9a565dae1cad9e17b2d461dd8749847fa
Hossein Bagheri <akaReza75@gmail.com>
2024-02-03 08:46:29 +0330

    Update aKaReZa.h

--- aKaReZa.h
+++ aKaReZa.h
@@ -1,25 +1,22 @@
-#ifndef _aKaReZa_H
-#define _aKaReZa_H
+#ifndef _aKaReZa_H_
+#define _aKaReZa_H_
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define delay_ms(delay_msValue) _delay_ms(delay_msValue)
-
-#define setBit(Reg, Bit) (Reg |= (1<<Bit))
-#define clearBit(Reg, Bit) (Reg &= ~(1<<Bit))
-#define toggleBit(Reg, Bit) (Reg ^= (1<<Bit))
-#define checkBit(Reg, Bit) ((Reg>>Bit) & 0x01)
+#define bitSet(Reg, Bit) (Reg |= (1<<Bit))
+#define bitClear(Reg, Bit) (Reg &= ~(1<<Bit))
+#define bitToggle(Reg, Bit) (Reg ^= 1<<Bit)
+#define bitCheck(Reg, Bit) ((Reg>>Bit) & 0x01)
+#define bitChange(Reg, Bit, Value) (Value == 1 ? bitSet(Reg, Bit) : bitClear(Reg, Bit))
 #define Conv_16to8_MSB(Value) (Value >> 8)
 #define Conv_16to8_LSB(Value) (Value & 0xFF)
 #define Combine_8to16(valueHigh, valueLow) (valueLow + (valueHigh<<8))
 
-int main(void)
-{
-  while(1)
-  {
+#define delay_ms(delay_msValue) _delay_ms(delay_msValue)
 
-  };
-};
+#define delay_ms(delay_msValue) _delay_ms(delay_msValue)
+#define GPIO_Config_OUTPUT(Reg, Bit) bitSet(Reg, Bit)
+#define GPIO_Config_INPUT(Reg, Bit) bitClear(Reg, Bit)
 
 #endif
