947c992c315396c8a17e614f184da6050d149c62
Hossein Bagheri <akaReza75@gmail.com>
2024-02-03 08:45:09 +0330

    Update Macros.c

--- Macros.c
+++ Macros.c
@@ -7,8 +7,12 @@
  *
  */
 
-#define setBit(Reg, Bit) (Reg |= (1<<Bit))
-#define clearBit(Reg, Bit) (Reg &= ~(1<<Bit))
-#define toggleBit(Reg, Bit) (Reg ^= 1<<Bit)
-#define checkBit(Reg, Bit) ((Reg>>Bit) & 0x01)
-#define changeBit(Reg, Bit, Value) (Value == 1 ? setBit(Reg, Bit) : clearBit(Reg, Bit))
+#define bitSet(Reg, Bit) (Reg |= (1<<Bit))
+#define bitClear(Reg, Bit) (Reg &= ~(1<<Bit))
+#define bitToggle(Reg, Bit) (Reg ^= 1<<Bit)
+#define bitCheck(Reg, Bit) ((Reg>>Bit) & 0x01)
+#define bitChange(Reg, Bit, Value) (Value == 1 ? bitSet(Reg, Bit) : bitClear(Reg, Bit))
+
+#define delay_ms(delay_msValue) _delay_ms(delay_msValue)
+#define GPIO_Config_OUTPUT(Reg, Bit) bitSet(Reg, Bit)
+#define GPIO_Config_INPUT(Reg, Bit) bitClear(Reg, Bit)
