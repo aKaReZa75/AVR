e106bec03dea401ba6c0d680f1e9f732ce3d3ad3
aKaReZa <aKaReZa75@gmail.com>
2024-04-15 20:06:38 +0330

    add main.c

---
+++ #AVR Resources/Libraries/main.c
@@ -0,0 +1,18 @@
+/* @file     main.c                                   */                      
+/* @brief    AVR microcontollers main  File           */
+/* @note     This file is for _AVR_ microcontrollers  */   
+/* @author   Hossein Bagheri                          */
+/* @github   aKaReZa75                                */
+/* @youtube  aKaReZa75                                */
+/* @linkedin aKaReZa75                                */
+/* @email    aKaReZa75@gmail.com                      */
+
+#include "aKaReZa.h"
+
+int main(void)
+{
+  while(1)
+  {
+
+  }'
+};
